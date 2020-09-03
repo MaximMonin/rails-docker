6b2a35720f95c2a511178cb7a41c865b8a903e3c
Maxim Monin <maximmonin@gmail.com>
2020-09-04 00:56:34 +0300

    setting up system test enviroment

--- image/app/test/application_system_test_case.rb
+++ image/app/test/application_system_test_case.rb
@@ -1,4 +1,25 @@
-require "test_helper"
+# reducing db recreation
+ActiveRecord::Base.maintain_test_schema = false
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+
+# clossing rails_dev db and open rails_test db
+ActiveRecord::Base.connection.close
+ENV['RAILS_ENV'] = 'test'
+Rails.env = "test"
+ActiveRecord::Base.establish_connection (:test)
+
+require 'rails/test_help'
+
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  # for system test we are using 1 thread and 1 database rails_test
+  parallelize(workers: 1)
+
+  # reset test data in rails_test db
+  fixtures :all
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1600, 1080], options: {
@@ -9,6 +30,8 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   }
   def setup
     super
+
+    # connecting to nginxtest and railstest containers
     Capybara.configure do |config|
       config.run_server = false
       config.app_host = 'http://nginxtest'
--- run_tests_system.sh
+++ run_tests_system.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker exec -it rails rails db:setup RAILS_ENV=test
-docker exec -it rails rails db:fixtures:load RAILS_ENV=test
-docker exec -it rails rails test:system PARALLEL_WORKERS=1
+#docker exec -it rails rails db:setup RAILS_ENV=test
+#docker exec -it rails rails db:fixtures:load RAILS_ENV=test
+docker exec -it rails rails test:system
