9a55d12f5a6ee660e05efedf84e339e8555fee93
Maxim Monin <maximmonin@gmail.com>
2020-09-12 04:24:35 +0300

    changed prometheus init

--- image/app/config/initializers/prometheus.rb
+++ image/app/config/initializers/prometheus.rb
@@ -1,37 +1,37 @@
 unless Rails.env == "test"
   require 'prometheus_exporter/middleware'
-  require 'prometheus_exporter/client'
 
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
-
   PrometheusExporter::Client.new(custom_labels: { hostname: ENV.fetch("VIRTUAL_HOST", "rails") })
+end
 
-  class Prometheus
-    include Singleton
+require 'prometheus_exporter/client'
 
-    def client
-      @client ||= PrometheusExporter::Client.default
+class Prometheus
+ include Singleton
+
+  def client
+    @client ||= PrometheusExporter::Client.default
+  end
+
+  def self.counters(*args)
+    instance.counters(*args)
+  end
+
+  def self.histograms(*args)
+    instance.histograms(*args)
+  end
+
+  def counters
+    @counters ||= Hash.new do |hash, key|
+      hash[key] = client.register(:counter, key, "count of #{key}")
     end
+  end
 
-    def self.counters(*args)
-      instance.counters(*args)
-    end
-
-    def self.histograms(*args)
-      instance.histograms(*args)
-    end
-
-    def counters
-      @counters ||= Hash.new do |hash, key|
-        hash[key] = client.register(:counter, key, "count of #{key}")
-      end
-    end
-
-    def histograms
-      @histograms ||= Hash.new do |hash, key|
-        hash[key] = client.register(:histogram, key, "histogram of #{key}")
-      end
+  def histograms
+    @histograms ||= Hash.new do |hash, key|
+      hash[key] = client.register(:histogram, key, "histogram of #{key}")
     end
   end
 end
